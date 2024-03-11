a8373912f20f7d0ee34068bd42c03efe5ca55f19
Christian Keegan <keeganchris91@gmail.com>
2024-03-11 13:24:11 -0300

    Added import logic

--- client/src/pages/import.jsx
+++ client/src/pages/import.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 export const ImportPage = () => {
+
+  const token = useSelector(state => state.session.token);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    let formData = new FormData(event.currentTarget);
+
+    fetch('http://127.0.0.1:8000/challenge/import/', {
+      method: 'POST',
+      headers: {
+        'Authorization': 'Token ' + token
+      },
+      body: formData,
+    })
+  }
 
   return (
     <div>
       <h2>Importar Elementos</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input name="file" type="file" />
+        </label>{" "}
+        <button type="submit">Enviar</button>
+      </form>
     </div>
   );
 };
